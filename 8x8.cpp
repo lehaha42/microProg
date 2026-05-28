ae5d95bd6ce933d0274f425de2f2c4ed5a0ad707
lehaha42 <146331806+lehaha42@users.noreply.github.com>
2026-05-28 18:09:52 +1000

    add MAP

--- 8x8.cpp
+++ 8x8.cpp
@@ -1,35 +1,36 @@
 #include <Arduino.h>
 
-int PINS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1};
+int PINS[] = {A0, A1, A2, A3, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+int MAP[] = {0, 5, 8, 3, 15, 9, 14, 11, 4, 13, 12, 1, 10, 2, 6, 7};
 
 void setup(){
   for(int i = 0; i<16; i++){
     pinMode(PINS[i], OUTPUT);
   };
   for (int i = 0; i<8; i++){
-    digitalWrite(PINS[i], LOW);
+    digitalWrite(PINS[MAP[i]], LOW);
   }
 }
 
 int arr[8] = {
-  0b10000001,
+  0b10011001,
   0b01000010,
   0b00100100,
-  0b00011000,
-  0b00011000,
+  0b10011001,
+  0b10011001,
   0b00100100,
   0b01000010,
-  0b10000001
+  0b10011001
 };
 
-int D = 10;
+int D = 800;
 void loop(){
   for (int i = 0; i<8; i++){
-    delay(D);
-    digitalWrite(PINS[(i-1)%8], HIGH);
+    digitalWrite(PINS[MAP[(i+7)%8]], LOW);
     for (int j = 0; j<8; j++){
-      digitalWrite(PINS[j+8], ((arr[i]&(1<<j))!=0) ? HIGH : LOW);
+      digitalWrite(PINS[MAP[j+8]], ((arr[i]&(1<<j))==0) ? HIGH : LOW);
     }
-    digitalWrite(PINS[i%8], LOW);
+    digitalWrite(PINS[MAP[i%8]], HIGH);
+    delayMicroseconds(D);
   }
 }
